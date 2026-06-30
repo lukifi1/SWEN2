f31d17d3fcc95fec50fd979f1fc9fe7f22cdd6dc
Lukas Fink <if24b248@technikum-wien.at>
2026-06-30 13:19:16 +0200

    chore: remove WebConfig and update main application class

--- server/src/main/java/at/fhtw/tourplanner/server/TourPlannerApplication.java
+++ server/src/main/java/at/fhtw/tourplanner/server/TourPlannerApplication.java
@@ -2,8 +2,11 @@ package at.fhtw.tourplanner.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
+@ConfigurationPropertiesScan
 public class TourPlannerApplication {
 
 	public static void main(String[] args) {
--- server/src/main/java/at/fhtw/tourplanner/server/config/WebConfig.java
+++
@@ -1,4 +0,0 @@
-package at.fhtw.tourplanner.server.config;
-
-public class WebConfig {
-}
